860fd19ffc8ece3aad70d48b974416769f822149
yash <yashsinghkhanpur7@gmail.com>
2022-10-12 19:06:56 +0530

    good

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int i, n, a[100], search,f;
+    int i, n, a[100], search,f=0;
     cout << "enter the size of array";
     cin >> n;
     cout << "enter the element of array";
@@ -18,9 +18,15 @@ int main()
         if (search == a[i])
         {
             cout << "element found" <<" "<< search;
+            f=1;
+            break;
             
         }
         
         
+    }
+    if(f==0)
+    {
+        cout<<"element not found";
     }
 }
